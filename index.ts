408410b55bb18b4a0d6ea50c15f200d5cb8bfd1f
gespina <glenn.espina@gmail.com>
2020-06-23 22:04:17 -0700

    Testing with true/false array

--- index.ts
+++ index.ts
@@ -1,23 +1,30 @@
 // Import stylesheets
 import "./style.css";
 
-const data = [2, 3, 4, 8, 11, 12, 34, 35, 36, 37];
+const data: number[] = [2, 3, 4, 8, 11, 12, 34, 35, 36, 37];
 
-let result = data
+const orig = [true, false, false, true, true, false, false, true, false, true, true, 
+false, false, false, false].reduce( (acc, cur, idx) => {
+  !cur ? acc.push(idx + 1) : null;
+  return acc;
+}, [])
+
+// debugger;
+let result = orig
   .reduce((acc, cur, idx, arr) => {
     let curArray = acc.pop() || [];
     let nextVal = arr[idx + 1] || 0;
     curArray.push(cur);
     acc.push(curArray);
-    if (nextVal - cur !== 1) {
+    if (nextVal !== 0 && nextVal - cur !== 1) {
       acc.push([]);
     }
     return acc;
   }, [])
-  .filter((x: any[]) => x.length > 0)
   .map((arr: any[]) =>
     arr.length > 1 ? `${arr[0]}-${arr[arr.length - 1]}` : `${arr[0]}`
-  );
+  )
+  .join(", ");
 
 console.log(result);
 // Write TypeScript code!
