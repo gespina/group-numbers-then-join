ea1e6f68ed06909bd59c0a9535b7bc87714b66ba
gespina <glenn.espina@gmail.com>
2020-06-23 21:34:48 -0700

    cleanup

--- index.ts
+++ index.ts
@@ -1,27 +1,25 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 const data = [2, 3, 4, 8, 11, 12, 34, 35, 36, 37];
 
-let result = data.reduce( (acc, cur, idx, arr) => {
-  // debugger;
-  let curArray = acc.pop() || [];
-  // console.log(curArray)
-  let nextVal = arr[idx + 1] || 0;
-  if(nextVal - cur === 1){
+let result = data
+  .reduce((acc, cur, idx, arr) => {
+    let curArray = acc.pop() || [];
+    let nextVal = arr[idx + 1] || 0;
     curArray.push(cur);
     acc.push(curArray);
-  } else {
-    curArray.push(cur)
-    acc.push(curArray);
-    acc.push([])
-  }
-  return acc;
-}, [] )
-.filter((x:any[]) => x.length > 0)
-.map( (arr:any[]) => arr.length > 1 ? `${arr[0]}-${arr[arr.length-1]}` : `${arr[0]}`)
+    if (nextVal - cur !== 1) {
+      acc.push([]);
+    }
+    return acc;
+  }, [])
+  .filter((x: any[]) => x.length > 0)
+  .map((arr: any[]) =>
+    arr.length > 1 ? `${arr[0]}-${arr[arr.length - 1]}` : `${arr[0]}`
+  );
 
-console.log(result)
+console.log(result);
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
